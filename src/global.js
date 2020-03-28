b88d3c2f7a660d625c6f1004c368451fd2895849
金戈宁 <jingening@gmail.com>
2020-03-28 09:02:24 +0800

    1

--- src/global.js
+++ src/global.js
@@ -1,7 +1,7 @@
-
+//全局文件
 const object = {
-    localhostUrl:'http://localhost:8078/',
-    wsHostUrl:'ws://localhost:8078/'
+    localhostUrl:'http://localhost:8078/',//后台地址
+    wsHostUrl:'ws://localhost:8078/',//websocket地址
 };
 
 export default object;
